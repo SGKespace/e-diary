f17c6be7576f87778531054fe1548ef21f10a1b5
SGK <55636018+SGKespace@users.noreply.github.com>
2023-03-10 10:28:54 +0400

    после ревью

--- scripts.py
+++ scripts.py
@@ -3,7 +3,7 @@ from django.db import models
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
 
 
-comments = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
+COMMENTS = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
             'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
             'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!',
             'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!',
@@ -34,14 +34,14 @@ def del_Chastisement():
     print('Шалость удалась!')
 
 
-def create_commendation(comments):
+def create_commendation(COMMENTS):
     schoolkid = name_check()
     target_lesson = input("Введите предмет: ")
     hack_lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject__title=target_lesson).order_by('?').first()
     if not hack_lesson:
         print("Что то пошло не так, ппоппробуйте еще раз")
         return
-    Commendation.objects.create(text=random.choice(comments), created=hack_lesson.date, schoolkid=schoolkid, subject=hack_lesson.subject, teacher=hack_lesson.teacher)
+    Commendation.objects.create(text=random.choice(COMMENTS), created=hack_lesson.date, schoolkid=schoolkid, subject=hack_lesson.subject, teacher=hack_lesson.teacher)
     print('Шалость удалась!')
 
 
